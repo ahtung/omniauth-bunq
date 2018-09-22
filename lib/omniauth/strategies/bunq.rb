f271a6a1ab1cff19a8373e743c4280d3c55721a4
Dunya Kirkali <dunyakirkali@gmail.com>
2018-09-22 20:53:31 +0200

    Clean up

--- lib/omniauth/strategies/bunq.rb
+++ lib/omniauth/strategies/bunq.rb
@@ -4,30 +4,10 @@ module OmniAuth
   module Strategies
     class Bunq < OmniAuth::Strategies::OAuth2
       option :client_options, {
-        :site => 'https://oauth.bunq.com/auth',
-        :authorize_url => 'https://oauth.bunq.com/auth',
-        :token_url => 'https://api.oauth.bunq.com/v1/token'
+        site: "https://oauth.bunq.com",
+        authorize_url: "/auth",
+        token_url: "https://api.oauth.bunq.com/v1/token"
       }
-
-      def request_phase
-        super
-      end
-
-      def authorize_params
-        super.tap do |params|
-          %w[scope client_options].each do |v|
-            if request.params[v]
-              params[v.to_sym] = request.params[v]
-            end
-          end
-        end
-      end
-
-      def token_params
-        super.tap do |params|
-          params[:grant_type] = "authorization_code"
-        end
-      end
 
       uid do
         puts options
